eebbfc2af94dfe74f0e3673aed8eaff9f9003d7c
Matt Filetto <matt.filetto@gmail.com>
2014-04-22 23:51:18 -0700

    Use dalvik heap targeted for 512mb devices
    Change-Id: I17b273814bde46af5ee6b0b712a6328e35615de6

--- holiday.mk
+++ holiday.mk
@@ -164,7 +164,7 @@ $(call inherit-product, device/htc/holiday/media_a1026.mk)
 $(call inherit-product, device/htc/holiday/media_htcaudio.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 # Device id
 PRODUCT_NAME := full_holiday
